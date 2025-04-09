02bfa97d75f308dc90905a0c23084817a00ffcbf
João Pedro <joaope.espindola2005@gmail.com>
2025-04-09 14:43:11 -0300

    Component: Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import './global.css'
+import styles from './App.module.css'
 
 import { Header } from './components/Header'
-
+import { Sidebar } from './components/Sidebar/Sidebar'
 export default function App() {
   return (
+   <div>
     <Header />
+    
+    <div className={styles.wrapper}>
+      <Sidebar/>
+      <main>
+        
+      </main>
+    </div>
+   </div>
   )
 }
 
