5570ba5f987e5a8f5ba8762bf5f185ec7571003d
João Pedro <joaope.espindola2005@gmail.com>
2025-04-10 12:29:47 -0300

    Component: Avatar

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,4 +1,5 @@
 import { Comment } from '../Comment/Comment'
+import { Avatar } from '../Avatar/Avatar'
 import styles from './Post.module.css'
 
 export function Post(){
@@ -6,7 +7,7 @@ export function Post(){
   <article className={styles.post}>
    <header>
     <div className={styles.author}>
-     <img src="https://github.com/diego3g.png"/>
+     <Avatar hasBorder src="https://github.com/diego3g.png"/>
      <div className={styles.authorInfo}>
       <strong>Diego Fernandes</strong>
       <span>Web developer</span>
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from '../Avatar/Avatar'
 import styles from './Sidebar.module.css'
 
 import { PencilSimpleLine } from "phosphor-react"
@@ -10,7 +11,8 @@ export function Sidebar(){
    src="https://images.unsplash.com/photo-1604964432806-254d07c11f32?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
    
    <div className={styles.profile}>
-    <img className={styles.avatar} src="https://github.com/espjotape.png"/>
+    <Avatar 
+    src="https://github.com/espjotape.png"/>
 
     <strong>João Pedro Espindola</strong>
     <span>Web Developer</span>
